380073a8a185a0e62e326df02ba5a603310fb53a
amirn <amirnaderi3059@yahoo.com>
2021-05-01 16:24:03 +0300

    Final commit

--- Project/__init__.py
+++ Project/__init__.py
@@ -7,17 +7,19 @@ class Elf:
         self.honesty = honesty
         self.index = index
 
+    def __repr__(self) -> str:
+        return "elf name: {}, honesty status: {}".format(self.index, self.honesty)
+
 
 def elves_creation():
     num_of_elves = int(input("enter the number of elves:"))
     for i in range(num_of_elves):
         # status = bool(random.getrandbits(1))
-        status = input("enter the " + str(i) + "th elf honesty status:\n (1 for honest and False for 0)")
+        status = input("enter the " + str(i) + "th elf honesty status:\n (1 for honest and False for 0)\n")
         if status == "1":
             status = True
         else:
             status = False
-        print(status)
         elf = Elf(status, i)
         elves.append(elf)
 
@@ -37,12 +39,14 @@ def an_honest_elf(honest_elves: list):
     potential_honest = []
     if len(honest_elves) == 1:
         return honest_elves[0]
-    for i in range(len(honest_elves) // 2):
-        state1, state2 = check_honesty(honest_elves[i], honest_elves[i + 1])
-        if state1 and state2:
-            potential_honest.append(honest_elves[i])
+    if len(honest_elves) == 2:
+        return honest_elves[1]
     if len(honest_elves) % 2 == 1:
         potential_honest.append(honest_elves[-1])
+    for i in range(len(honest_elves) // 2):
+        state1, state2 = check_honesty(honest_elves[2*i], honest_elves[2*i + 1])
+        if state1 and state2:
+            potential_honest.append(honest_elves[i])
     return an_honest_elf(potential_honest)
 
 
@@ -51,7 +55,7 @@ def all_honest_elves(the_chosen_one: Elf):
     for i in elves:
         if the_chosen_one.index != i.index:
             st1, st2 = check_honesty(the_chosen_one, i)
-            if st2:
+            if st1:
                 result[i.index] = "honest"
             else:
                 result[i.index] = "liar"
@@ -60,5 +64,6 @@ def all_honest_elves(the_chosen_one: Elf):
 
 elves = []
 elves_creation()
+# print(elves)
 dobby = an_honest_elf(elves)
 print(all_honest_elves(dobby))
